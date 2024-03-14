a3b5e1f7f999a60cc287d587013b346cb8d18702
Evernayt <110179244+Evernayt@users.noreply.github.com>
2024-03-14 10:18:16 +0500

    Add files via upload

---
+++ Shutdown Timer/ShutdownForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Shutdown_Timer
+{
+    partial class ShutdownForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShutdownForm));
+            this.nudHours = new System.Windows.Forms.NumericUpDown();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.nudMinutes = new System.Windows.Forms.NumericUpDown();
+            this.nudSeconds = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSeconds)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudHours
+            // 
+            this.nudHours.Location = new System.Drawing.Point(12, 30);
+            this.nudHours.Name = "nudHours";
+            this.nudHours.Size = new System.Drawing.Size(122, 23);
+            this.nudHours.TabIndex = 0;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.btnExecute.FlatAppearance.BorderSize = 2;
+            this.btnExecute.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnExecute.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnExecute.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExecute.Location = new System.Drawing.Point(12, 63);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(122, 30);
+            this.btnExecute.TabIndex = 3;
+            this.btnExecute.TabStop = false;
+            this.btnExecute.Text = "Выполнить";
+            this.btnExecute.UseVisualStyleBackColor = false;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatAppearance.BorderColor = System.Drawing.Color.Brown;
+            this.btnCancel.FlatAppearance.BorderSize = 2;
+            this.btnCancel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(171)))), ((int)(((byte)(173)))));
+            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(171)))), ((int)(((byte)(173)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Location = new System.Drawing.Point(140, 63);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(122, 30);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.TabStop = false;
+            this.btnCancel.Text = "Отменить";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // nudMinutes
+            // 
+            this.nudMinutes.Location = new System.Drawing.Point(140, 30);
+            this.nudMinutes.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.nudMinutes.Name = "nudMinutes";
+            this.nudMinutes.Size = new System.Drawing.Size(58, 23);
+            this.nudMinutes.TabIndex = 1;
+            // 
+            // nudSeconds
+            // 
+            this.nudSeconds.Location = new System.Drawing.Point(204, 30);
+            this.nudSeconds.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.nudSeconds.Name = "nudSeconds";
+            this.nudSeconds.Size = new System.Drawing.Size(58, 23);
+            this.nudSeconds.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Часов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Минут";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Секунд";
+            // 
+            // ShutdownForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(275, 104);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudSeconds);
+            this.Controls.Add(this.nudMinutes);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.nudHours);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ShutdownForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shutdown Timer";
+            ((System.ComponentModel.ISupportInitialize)(this.nudHours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSeconds)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudHours;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.NumericUpDown nudMinutes;
+        private System.Windows.Forms.NumericUpDown nudSeconds;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Shutdown Timer/ShutdownForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Shutdown_Timer
+{
+    public partial class ShutdownForm : Form
+    {
+        public ShutdownForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            if (nudHours.Value != 0 | nudMinutes.Value != 0 | nudSeconds.Value != 0)
+            {
+                int total = Convert.ToInt32((nudHours.Value * 3600) + (nudMinutes.Value * 60) + nudSeconds.Value);
+
+                Cmd($"-s -t { total }");
+
+                MessageBox.Show($"Компьютер выключиться через:\n{ nudHours.Value } час., { nudMinutes.Value } мин. и { nudSeconds.Value } сек.",
+                    Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Значение не введено.\nТаймер не будет запущен.", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Cmd("-a");
+            MessageBox.Show("Таймер отменен.\nКомпьютер не завершит работу.", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Cmd(string line)
+        {
+            Process.Start(new ProcessStartInfo
+            { 
+                FileName = "cmd",
+                Arguments = $"/C shutdown { line }",
+                WindowStyle = ProcessWindowStyle.Hidden 
+            });
+        }
+    }
+}
